a29bd08489e74bf2383f4ce9a1175d9bf31852a9
VladScv <ddcllrnsbrv@uoc.edu>
2022-06-10 22:18:38 +0200

    change img order

--- slide.js
+++ slide.js
@@ -1,5 +1,31 @@
-let slideIndex = 0;
-showSlides();
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
 let playText = false;
 let textIndex=0;
 const text=['COMPRA TUS SUEÑOS' ,
@@ -281,18 +307,6 @@ const text=['COMPRA TUS SUEÑOS' ,
 const playBtn = document.querySelector(".play");
 
 const manifestBtn = document.querySelector(".manifest-btn");
-function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 3500); // Change image every 2 seconds
-}
-
 const consoleOut = document.querySelector('.console');
 
 const outputMessage = function (str) {
